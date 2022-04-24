0559e27864c3ea680240b8543d8ef0f13ce8c6eb
Ahmed El Gabalawy <42101568+ahmedelgabalawy@users.noreply.github.com>
2022-04-24 10:21:58 +0200

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function toggle(id) {
+    var x = document.getElementById(id);
+    x.classList.toggle('d-none')
+  }
+
+  function showImage(containerId , img) {
+    var container = document.getElementById(containerId)
+    container.src=img
+  }
